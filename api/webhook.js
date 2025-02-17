9e533ef8e851a724eae796222bc9ead7609d5b9d
Konstantin Gabelok <constantingab@gmail.com>
2025-02-17 18:54:50 +0300

    fix: fixiki

--- api/webhook.js
+++ api/webhook.js
@@ -6,7 +6,11 @@ module.exports = async (req, res) => {
   console.log("Request body:", req.body);
 
   try {
-    await webhookCallback(bot, "https")(req, res); // Указываем протокол явно
+    if (req.method === "POST") {
+      await webhookCallback(bot, "https")(req, res);
+    } else {
+      res.status(405).send("Method Not Allowed");
+    }
   } catch (error) {
     console.error("Error in webhook handler:", error);
     res.status(500).send("Internal Server Error");

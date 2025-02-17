e2d508fba85d70167b262715707d08bdfc2a41dc
Konstantin Gabelok <constantingab@gmail.com>
2025-02-17 18:31:44 +0300

    fix: fix problems

--- api/webhook.js
+++ api/webhook.js
@@ -1,12 +1,14 @@
+const { bot } = require("../lib/bot"); // Добавляем импорт бота
+const { webhookCallback } = require("grammy");
+
 module.exports = async (req, res) => {
-    console.log("Received webhook:", req.method, req.url); // Логирование запроса
-    console.log("Request body:", req.body); // Логирование тела запроса
+  console.log("Received webhook:", req.method, req.url);
+  console.log("Request body:", req.body);
 
-    try {
-      await webhookCallback(bot)(req, res);
-    } catch (error) {
-      console.error("Error in webhook handler:", error);
-      res.status(500).send("Internal Server Error");
-    }
-  };
-
+  try {
+    await webhookCallback(bot)(req, res);
+  } catch (error) {
+    console.error("Error in webhook handler:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};

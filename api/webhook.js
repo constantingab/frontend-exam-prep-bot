90dcc453acb6dc6f0759bdea626228745f716ca9
Konstantin Gabelok <constantingab@gmail.com>
2025-02-17 15:15:16 +0300

    fix: fix redirection problem in webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,4 +1,13 @@
 const { webhookCallback } = require('grammy');
 const { bot } = require('../lib/bot');
 
-module.exports = webhookCallback(bot, 'http');
+module.exports = async (req, res) => {
+  console.log('Received webhook:', req.method, req.url);
+
+  try {
+    await webhookCallback(bot, 'http')(req, res);
+  } catch (error) {
+    console.error('Error in webhook handler:', error);
+    res.status(500).send('Internal Server Error');
+  }
+};

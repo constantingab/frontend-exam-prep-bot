00f4b30fdfaf0c190d4ed568d4118856d818c97c
Konstantin Gabelok <constantingab@gmail.com>
2025-02-17 15:55:55 +0300

    fix: fix problem with 500 error

--- api/webhook.js
+++ api/webhook.js
@@ -5,7 +5,9 @@ module.exports = async (req, res) => {
   console.log("Received webhook:", req.method, req.url);
 
   try {
+    console.log("Processing request...");
     await webhookCallback(bot, "http")(req, res);
+    console.log("Request processed successfully");
   } catch (error) {
     console.error("Error in webhook handler:", error);
     res.status(500).send("Internal Server Error");

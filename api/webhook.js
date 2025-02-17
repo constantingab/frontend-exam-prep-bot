d739d9799b36d55188342f5ab3fef82765adb267
Konstantin Gabelok <constantingab@gmail.com>
2025-02-17 18:39:26 +0300

    fix: something

--- api/webhook.js
+++ api/webhook.js
@@ -1,4 +1,4 @@
-const { bot } = require("../lib/bot"); // Добавляем импорт бота
+const { bot } = require("../lib/bot");
 const { webhookCallback } = require("grammy");
 
 module.exports = async (req, res) => {
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
   console.log("Request body:", req.body);
 
   try {
-    await webhookCallback(bot)(req, res);
+    await webhookCallback(bot, "https")(req, res); // Указываем протокол явно
   } catch (error) {
     console.error("Error in webhook handler:", error);
     res.status(500).send("Internal Server Error");
